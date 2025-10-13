bd8665672f50d3410c907cac9ea2af7530b6cfad
Yasemin <ysmnklml@gmail.com>
2025-10-13 12:27:48 +0200

    Update WordAnalysisTool.py

--- WordAnalysisTool.py
+++ WordAnalysisTool.py
@@ -2,16 +2,16 @@
 
 sentence = input("Please enter a sentence: ")
 
-character_count = len(sentence)
+character_count = len(sentence.replace(" ", ""))
 print("Number of characters:", character_count)
 
-word_count = len(sentence.split())
-print("Number of words:", word_count)
+word_count = sentence.split()
+print("Number of words:", len(word_count))
 
-unique_words = set(sentence.split())
-print (unique_words)
+unique_words = set(word_count)
+print ("Unique_words:", unique_words)
 
-longest_word = max(sentence.split(), key=len)
+longest_word = max(word_count , key = len)
 print("Longest word:", longest_word)
 
 
